b89d3c3effb596541f072fa40dd94af27d4cd126
Lukewh <luke@lukewh.com>
2012-09-04 17:08:47 +0100

    First commit of v1 of ReadyListener

---
+++ ReadyListener.js
@@ -0,0 +1,21 @@
+var ReadyListener = function(itemLength, fn){
+	this.itemLength = itemLength;
+	this.haveItems = 0;
+	this.fn = fn;
+};
+
+ReadyListener.prototype.add = function(name){
+	console.log('	'+name+': Ready');
+	this.haveItems++;
+	this.haveAllCheck();
+};
+
+ReadyListener.prototype.haveAllCheck = function(){
+	if(this.haveItems === this.itemLength){
+		this.fn();
+	}
+};
+
+if(module && module.exports){
+	module.exports = ReadyListener;
+}
